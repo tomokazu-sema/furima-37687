14538f6aceae1f99e3311d20965adcf2d5a3d8e5
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-15 12:36:13 +0900

    ログイン中のみ出品ページに移動可能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: :new
+
   def index
   end
 
   def new
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
