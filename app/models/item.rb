af6245e088468e2a89367162a5fd8d9a9d4a22cb
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-15 17:50:13 +0900

    ActiveHash記載漏れ修正、バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :category
   belongs_to :sales_status
@@ -10,15 +11,14 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :info
-    with_options numericality: { other_than: 1, message: "can't be blank" } do
-      validates :category_id
-      validates :sales_status_id
-      validates :shipping_fee_status_id
-      validates :prefecture_id
-      validates :scheduled_delivery_id
-    end
-    validates :price, 
-              numericality: { in: 300..9999999 },
-              format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
+    validates :price, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
+  end 
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' }
+  with_options presence: true, numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
   end
 end
