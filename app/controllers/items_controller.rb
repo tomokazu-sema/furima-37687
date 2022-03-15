8debe6ff73309a2c0a227b356d1357b7821b3a26
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-15 17:52:21 +0900

    createアクション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
 
   def move_to_index
     unless user_signed_in?
