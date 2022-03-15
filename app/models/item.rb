3148c9cb6377b61efd52ed85b062dfaae5c96a3d
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-15 14:25:17 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :price, 
+              numericality: { in: 300..9999999 },
+              format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
+  end
 end
