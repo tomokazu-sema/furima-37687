c474672c242d41d605eab13d76f2b6cda643b102
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-15 15:13:40 +0900

    ActiveHashのアソシエーション設定、バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,22 @@
 class Item < ApplicationRecord
   belongs_to :user
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
   has_one_attached :image
   with_options presence: true do
     validates :image
     validates :name
     validates :info
-    validates :category_id
-    validates :sales_status_id
-    validates :shipping_fee_status_id
-    validates :prefecture_id
-    validates :scheduled_delivery_id
+    whit_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id
+      validates :sales_status_id
+      validates :shipping_fee_status_id
+      validates :prefecture_id
+      validates :scheduled_delivery_id
+    end
     validates :price, 
               numericality: { in: 300..9999999 },
               format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
