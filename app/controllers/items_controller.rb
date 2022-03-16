64b6d15f2d31c550c9016e9ac341341ba0ff64f2
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-16 18:41:27 +0900

    createアクションへの制限が漏れていたため修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
 
   def index
