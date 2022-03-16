0df1ed728054743dbbb6ff200a61cdfe3ef28b2d
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-16 19:49:21 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,8 +48,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless current_user.id == get_item.user_id
-      redirect_to root_path
-    end
+    redirect_to root_path unless current_user.id == get_item.user_id
   end
 end
