db4a52156c20d3ff117720273f0212a43b107cfd
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-17 15:39:55 +0900

    createアクションを記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :set_item
+
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -12,4 +12,9 @@ class OrderAddress
     validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number' }
   end
   validates :phone_number, format:{ with: /\A[0-9]{10,11}\z/, message: 'is too short' }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+  end
 end
