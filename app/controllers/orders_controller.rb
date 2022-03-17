60bb0ef8db044293dcf988bc56722b88ccca2f84
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-17 11:07:49 +0900

    createアクション作成、ルーティング設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,7 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
-    resources :orders, only: :index
+    resources :orders, only: [:index, :create]
   end
 end
