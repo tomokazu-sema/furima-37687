0855fac64ebf92c5ad95742ff10c6005e8630151
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-17 17:20:19 +0900

    FactoryBotのorder_addressインスタンス定義

--- spec/factories/orders_address.rb
+++ spec/factories/orders_address.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :order_address do
-    
+    token { 'tok_abcdefghijk00000000000000000' }
+    postal_code { '123-4567' }
+    city { '東京市' }
+    addresses { '東京1-1' }
+    building { '東京アパート' }
+    phone_number { '09012345678' }
   end
 end
