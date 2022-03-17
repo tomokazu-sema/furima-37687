3874db421ffa95b4481f1e228a43290efd0e78dd
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-17 15:14:08 +0900

    バリデーションの設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,15 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :token, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :token
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)'}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :addresses
+    validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number' }
+  end
+  validates :phone_number, format:{ with: /\A[0-9]{10,11}\z/, message: 'is too short' }
 end
