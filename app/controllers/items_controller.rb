40b22745e9e4d0a5ac4eaf5f160010da9cf7e46b
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-17 19:21:30 +0900

    商品情報編集機能の購入済みの場合のページ遷移制限実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,6 +52,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless current_user.id == Item.find(params[:id]).user_id
+    item = Item.find(params[:id])
+    redirect_to root_path if current_user.id != item.user_id || item.order
   end
 end
